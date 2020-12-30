a7a80fd3a9996ef186f355f85bfc9e81731ad911
Aman Pratap <64463155+donot-liketoCODE@users.noreply.github.com>
2020-12-30 12:40:58 +0530

    Create 4A_watermelon.cpp

---
+++ 4A_watermelon.cpp
@@ -0,0 +1,14 @@
+//By cp_days, contest: Codeforces Beta Round #4 (Div. 2 Only), problem: (A) Watermelon, Accepted
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	int n;
+	cin>>n;
+	if( n%2==0 && n!=2)
+		cout<<"YES";
+	else
+		cout<<"NO";
+	return 0;
+}
