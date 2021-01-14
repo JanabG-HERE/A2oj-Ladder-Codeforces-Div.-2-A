c0bd4d49ecde50db8f50df9e681e80ec8896ff57
Aman Pratap <amanpratap1026@gmail.com>
2021-01-14 11:16:50 +0530

    Create Wizard of Orz.cpp

---
+++ Wizard of Orz.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <set>
+#define ll long long
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+	ll tost;
+	cin>>tost;
+	
+	while(tost--)
+	{
+		ll no;
+		cin>>no;
+		cout<<"9";
+		for(ll i=0;i<no-1;i++)
+		{
+			cout<<(8+i)%10;
+				
+		}
+		cout<<endl;
+	}
+	return
